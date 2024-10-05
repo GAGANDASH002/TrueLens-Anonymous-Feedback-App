d14b6d8505f689e379909a8317f47476d956f2d4
GAGANDASH002 <gagandash002@gmail.com>
2024-10-05 17:52:07 +0530

    validate unique username, verify code via email

--- src/helpers/sendVerificationEmail.ts
+++ src/helpers/sendVerificationEmail.ts
@@ -11,7 +11,7 @@ export async function sendVerificationEmail(
     await resend.emails.send({
       from: 'Acme <onboarding@resend.dev>',
       to: email,
-      subject: 'Mystery Message Verification Code',
+      subject: 'TrueLens Verification Code',
       react: VerificationEmail({ username, otp: verifyCode }),
     });
     return { success: true, message: 'Verification email sent successfully.' };
