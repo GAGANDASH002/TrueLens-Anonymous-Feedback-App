42dcafc86303e1bfeca46bd67bc5753474e8bd5a
GAGANDASH002 <gagandash002@gmail.com>
2024-10-10 21:36:25 +0530

    add sign-in and verification code ui page

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,7 +1,7 @@
 import type { Metadata } from "next";
 import localFont from "next/font/local";
 import "./globals.css";
-import AuthProvider from "@/context/AuthProvider";
+import AuthProvider from "@/context/AuthProvider"; 
 import { Toaster } from "@/components/ui/toaster";
 
 const geistSans = localFont({
